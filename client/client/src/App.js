84c7e69ab77c972e73b86acbad97a9f29ba3d266
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-03-03 13:29:19 +0000

    added index.css

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
 import {
-  BrowserRouter as BrowserRouter,
+  BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
 
 import Header from './components/Header';
+import Public from './components/Public';
 import NotFound from './components/NotFound';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import SignOut from './components/SignOut';
 import Course from './components/Course';
+import CourseDetail from './components/CourseDetail';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
-
 import withContext from './Context';
-import PrivateRoute from './PrivateRoute';
+// import PrivateRoute from './PrivateRoute';
 
 const HeaderWithContext = withContext(Header);
-const AuthWithContext = withContext(Authenticated);
+// const AuthWithContext = withContext(Authenticated);
 const SignUpWithContext = withContext(SignUp);
 const SignInWithContext = withContext(SignIn);
 const SignOutWithContext = withContext(SignOut);
@@ -34,7 +36,7 @@ export default () => (
 
       <Switch>
         <Route exact path="/" component={Public} />
-        <PrivateRoute path="/authenticated" component={AuthWithContext} />
+        {/* <PrivateRoute path="/authenticated" component={AuthWithContext} /> */}
         <Route path="/signin" component={SignInWithContext} />
         <Route path="/signup" component={SignUpWithContext} />
         <Route path ="/signout" component={SignOutWithContext} />
