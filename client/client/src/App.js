7956d8fde66d01dd09d9fd2a7e0698b6189c0223
JackM1994 <36908600+JackM1994@users.noreply.github.com>
2020-03-03 09:05:32 +0000

    added component files

---
+++ client/client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {
+  BrowserRouter as BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Course from './components/Course';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+
+import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
+
+const HeaderWithContext = withContext(Header);
+const AuthWithContext = withContext(Authenticated);
+const SignUpWithContext = withContext(SignUp);
+const SignInWithContext = withContext(SignIn);
+const SignOutWithContext = withContext(SignOut);
+const CourseWithContext = withContext(Course);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+
+
+export default () => (
+  <Router>
+    <div>
+      <HeaderWithContext />
+
+      <Switch>
+        <Route exact path="/" component={Public} />
+        <PrivateRoute path="/authenticated" component={AuthWithContext} />
+        <Route path="/signin" component={SignInWithContext} />
+        <Route path="/signup" component={SignUpWithContext} />
+        <Route path ="/signout" component={SignOutWithContext} />
+        <Route path="/courses" component={CourseWithContext} />
+        <Route path="/courses/create" component ={CreateCourseWithContext}/>
+        <Route path="/courses/:id/update" component={UpdateCourseWithContext}/>
+        <Route path="courses/:id-CourseDetail" component={CourseDetail} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
